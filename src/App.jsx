2545c9e721772eed8f3a14a18ebf07db63246b5a
Nicolas Oten <nicolas.wt@gmail.com>
2023-08-30 00:03:00 -0300

    initial commit for Neurona project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='logoHolder'>
+      <img className='isologo' src="../src/assets/favicon.png" alt="" />
+      <img className='logo' src="../src/assets/neurona_logo.png" alt="" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+	return ( 
+		<>
+			<div className="logo">
+				<img src="../assets/neurona_logo.png" alt="" />
+			</div>
+		</div>
+	);
+}
+ 
+export default Navbar;
